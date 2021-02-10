31ea556e171039c2e6fb93b4f496be281b563e1e
Marijn Ploeg <marijnploeg@gmail.com>
2021-02-10 19:53:52 +0100

    Loop over launches and log

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 async function displayLaunches() {
     const response = await axios.get('https://api.spacexdata.com/v4/launches')
-    console.log(response)
 
     const launchList = document.getElementById('spaceX-launches')
+
+    for (const launch of response.data) {
+        console.log(launch)
+    }
 }
 
 displayLaunches();

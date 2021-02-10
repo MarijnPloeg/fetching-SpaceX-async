053e9dc7384a26aeec98716566e47d598341981a
Marijn Ploeg <marijnploeg@gmail.com>
2021-02-10 19:51:32 +0100

    added ul to html and select in async func

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 async function displayLaunches() {
     const response = await axios.get('https://api.spacexdata.com/v4/launches')
     console.log(response)
+
+    const launchList = document.getElementById('spaceX-launches')
 }
 
 displayLaunches();
